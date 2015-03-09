587dc39bccc666dccf5c98a474a977dfe9d8d6fc
yiyangyi <yiyangyigg@gmail.com>
2015-03-09 21:00:25 +0800

    Basic application controller, more to be added.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,39 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :unread_notify_count
+
+  def set_seo_meta(title = '', meta_keyword = '', meta_description = '')
+  	@page_title = "#{title}" if title.length > 0
+  	@meta_keyword = meta_keyword
+  	@meta_description = meta_description
+  end	
+
+  def require_user
+  	if current_user.blank?
+  		respond_to do |format|
+  			format.html { authenticate_user! }
+  			format.all { head(:unauthorized) }
+  	end
+  end
+
+  def unread_notify_count
+  	return 0 if current_user.blank?
+  	@unread_notify_count ||= current_user.notifications.unread.count
+  end
+
+  def render_optional_error_file(status_code)
+  	status = status_code.to_s
+  	fname = %W(403 404 422 500).include?(status) ? status : 'unknow'
+  	render template: "/errors/#{fname}", format: [:html],
+  				 handler: [:erb], status: status, layout: 'application'
+  end
+
+  def render_403
+  	render_optional_error_file(403)
+  end
+
+  def render_404
+  	render_optional_error_file(404)
+  end
+  
 end
