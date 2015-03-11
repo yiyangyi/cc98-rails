e6ff2beaa9ae670bf89669aed73db79a041474d1
yiyangyi <yiyangyigg@gmail.com>
2015-03-11 11:41:29 +0800

    The model used to complement the topic.

---
+++ app/models/append.rb
@@ -0,0 +1,3 @@
+class Append
+  include Mongoid::Document
+end
---
+++ test/models/append_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AppendTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
