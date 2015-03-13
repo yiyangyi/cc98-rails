209782bf09c5746c3cf74d3281c6b50a813f24c7
yiyangyi <yiyangyigg@gmail.com>
2015-03-13 21:26:30 +0800

    Define the basic ability to restrict what resources a given user is allowed to access.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,31 +2,27 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    if user.blank?
+      cannot :manage, :all
+      basic_ability
+    elsif user.has_role?(:admin)
+      can :manage, :all
+    elsif user.has_role?(:member)     
+    else
+      cannot :manage, :all
+      basic_ability
+    end
+    protected
+      def basic_ability
+        can :read, Topic
+        can :feed, Topic
+        can :node, Topic
+        can :read, Reply
+        can :read, Photo
+        can :read, Section
+        can :read, Node
+        can :read, Comment
+        can :preview, Note
+      end
   end
 end
