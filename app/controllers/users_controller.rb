1c540bb05c48178d8d06d0c2fdda64cae96e0d09
yiyangyi <yiyangyigg@gmail.com>
2015-03-14 11:34:20 +0800

    A little mistake, that should be in model not in controller. :)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,42 +4,4 @@ class UsersController < ApplicationController
 
   def show
   end
-
-  STATE = { deleted: -1, normal: 1, blocked: 2 }
-
-  def newbie?
-  	return false if self.verified == true
-  	self.created_at < 1.week.ago
-  end
-
-  def deleted?
-  	return self.state == STATE[:deleted]
-  end
-
-  def soft_delete
-  	self.email = "#{self.login}_#{self.id}@cc98.org"
-  	self.login = "Guest"
-  	self.bio = ""
-  	self.website = ""
-  	self.tagline = ""
-  	self.location = ""
-  	self.state = STATE[:deleted]
-  	self.save(validate: false)
-  end
-
-  def blocked?
-  	return self.state == STATE[:blocked]
-  end
-
-  def admin?
-  	Setting.admin_email.include?(self.email)
-  end
-
-  def has_role?(role)
-  	case role
-  	  when :admin then admin?
-  	  when :member then self.state = STATE[:normal]
-  	  else false
-  	end
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   ## Database authenticatable
   field :email,              type: String, default: ""
@@ -33,4 +33,42 @@ class User
   # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
+  
+  STATE = { deleted: -1, normal: 1, blocked: 2 }
+
+  def newbie?
+    return false if self.verified == true
+    self.created_at < 1.week.ago
+  end
+
+  def deleted?
+    return self.state == STATE[:deleted]
+  end
+
+  def soft_delete
+    self.email = "#{self.login}_#{self.id}@cc98.org"
+    self.login = "Guest"
+    self.bio = ""
+    self.website = ""
+    self.tagline = ""
+    self.location = ""
+    self.state = STATE[:deleted]
+    self.save(validate: false)
+  end
+
+  def blocked?
+    return self.state == STATE[:blocked]
+  end
+
+  def admin?
+    Setting.admin_email.include?(self.email)
+  end
+
+  def has_role?(role)
+    case role
+      when :admin then admin?
+      when :member then self.state = STATE[:normal]
+      else false
+    end
+  end
 end
