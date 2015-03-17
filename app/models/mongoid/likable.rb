4e6d42d48be37b0aa3fe1d3b53fffed6c128f732
yiyangyi <yiyangyigg@gmail.com>
2015-03-17 10:50:21 +0800

    Add the likable module.

---
+++ app/models/mongoid/likable.rb
@@ -0,0 +1,15 @@
+module Mongoid
+  module Likable
+    extend Activesupport::Concern
+
+    included do
+      field :liked_user_ids, type: Array,   default: []
+      field :likes_count,    type: Integer, default: 0
+    end
+
+    def liked_by_user?(user)
+      return false if user.blank?
+      liked_user_ids.include?(user.id)
+    end
+  end
+end
