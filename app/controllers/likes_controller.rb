d5c3a3a91a5d109bdf6222295535e4c4ee545cd7
yiyangyi <yiyangyigg@gmail.com>
2015-03-17 18:55:54 +0800

    Add the find_likable before_action to find something able to the liked based on the params.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,37 @@ class LikesController < ApplicationController
   before_action :find_likeable
   
   def create
+    current_user.like(@item)
+    render text: @item.reload.likes_count
   end
 
   def destroy
+    current_user.unlike(@item)
+    render text: @item.reload.likes_count
   end
 
   private
   def find_likeable
+    @success = false
+    @element_id = "likable_#{params[:type]}_#{params[:id]}"
+    unless params[:type].in?(%W(Topic Reply))
+      render text: '-1'
+      false
+    end
+
+    case @params[:type].downcase
+      when 'topic'
+        klass = Topic
+      when 'reply'
+        klass = Reply
+      else
+        false
+    end
+
+    @item = klass.find_by_id(params[:id])
+    if @item.blank?
+      render text: '-2'
+      false
+    end
   end
 end
