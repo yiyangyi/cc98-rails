8f26c1db2c361b7d0212c32c514fd65b71fb12d2
yiyangyi <yiyangyigg@gmail.com>
2015-03-17 20:05:29 +0800

    Modified and configured the mailer class to send emails.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
+  default charset: "utf-8"
+  default content_type: "text/html"
+
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+  def welcome
+    @user = User.find_by_id(params[:id])
+    return false if @user.blank?
+    mail(to: @user.email, subject: "")
+  end
 end
