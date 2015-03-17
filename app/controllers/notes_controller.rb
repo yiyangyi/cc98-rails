4dad06ab2298043dbb4dd3323c9835d7218c9a1e
yiyangyi <yiyangyigg@gmail.com>
2015-03-17 19:18:53 +0800

    Add the notes controller for current user to do basic CRUD operations.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,26 +1,48 @@
 class NotesController < ApplicationController
   def index
+    @notes = current_user.notes.recent_updated
   end
 
   def show
+    @note = Note.find(params[:id])
+    @note.hits.inc(1)
   end
 
   def new
+    @note = current_user.notes.build
   end
 
   def create
+    @note = current_user.notes.new(note_params)
+    @note.publish = note_params[:publish] == '1'
+    if @note.save()
+      redirect_to(notes_path, notice: "New note created successfully!")
+    else
+      render action: :new
+    end
   end
 
   def edit
+    @note = current_user.notes.find_by_id(params[:id])
   end
 
   def update
+    @note = current_user.notes.find_by_id(params[:id])
+    if @note.update_attributes(note_params)
+      redirect_to(notes_url)
+    else
+      render action: :edit
+    end
   end
 
   def destory
+    @note = current_user.notes.find_by_id(params[:id])
+    @note.destroy
+    redirect_to(notes_url)
   end
 
   def preview
+    render text: MarkdownConverter.convert(params[:body])
   end
 
   private
