f07c2d5b647468d01a5e4c43564443010f1caceb
yiyangyi <yiyangyigg@gmail.com>
2015-03-18 21:18:26 +0800

    Add the create method for uploading the photo and return the url of the photo to the form.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,9 @@
 class PhotosController < ApplicationController
   def create
+    @photo = Photo.new
+    @photo.photo = params[:Filedata]
+    @photo.user_id = current_user.id
+    @photo.save
+    render text: @photo.photo.url
   end
 end
