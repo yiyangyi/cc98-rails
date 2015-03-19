a621be2b81dc2c164cc0a35185605f3cbcedd6de
yiyangyi <yiyangyigg@gmail.com>
2015-03-19 19:19:16 +0800

    Set up the many to many relationships between users and nodes.

--- app/models/node.rb
+++ app/models/node.rb
@@ -10,6 +10,8 @@ class Node
   has_many :topics
   belongs_to :section
 
+  has_and_belongs_to_many :followers, class_name: 'User', inverse_of: :following_nodes
+
   index section_id: 1
 
   scope :hots, -> { desc(:topic_count) }
