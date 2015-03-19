1a6982cec0b9a0c1f410d84ae4759148af70c1ab
yiyangyi <yiyangyigg@gmail.com>
2015-03-19 09:57:44 +0800

    Add two before method to the note model.

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,9 @@ class Note
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  before_save :auto_set_value
+  before_update :update_changes_count
+
   field :title
   field :body
   field :word_count, type: Integer
@@ -15,4 +18,16 @@ class Note
 
   scope :published, -> { where(publish: true) }
   scope :recent_updated, -> { desc(updated_at) }
+
+  def auto_set_value
+    if !self.body.blank?
+      self.title = self.body.split("\n").first[0..50]
+      self.word_count = self.body.length
+    end
+  end
+
+  def update_changes_count
+    self.changes_count = 0 if self.changes_count.blank?
+    self.inc(changes_count: 1.)
+  end
 end
