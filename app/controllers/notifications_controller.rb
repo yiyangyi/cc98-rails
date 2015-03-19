207491ce05b921038abf81f074b777a4aabfc31a
yiyangyi <yiyangyigg@gmail.com>
2015-03-19 11:11:23 +0800

    Add the method to clear or destroy the notifications.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,10 +1,34 @@
 class NotificationsController < ApplicationController
-  def index
-  end
+  before_action :require_user
 
-  def destroy
+  def index
+    @notifications = current_user.notifications.recent
+    current_user.read_notifications(@notifications)
   end
 
   def clear
+    current_user.notifications.delete_all
+    respond_with do |format|
+      format.html { redirect_to notifications_path }
+      format.js { render layout: false }
+    end
+  end
+
+  def delete
+    @notification = current_user.notifications.find(params[:id])
+    @notification.delete
+    respond_with do |format|
+      format.html { redirect_to notifications_path }
+      format.js { render layout: false }
+    end
+  end
+
+  def destroy
+    @notification = current_user.notifiacations.find(params[:id])
+    @notification.destroy
+    respond_with do |format|
+      format.html { redirect_to notifications_path }
+      format.js { render layout: false }
+    end
   end
 end
