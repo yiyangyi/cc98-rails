28a8b3e91dc0f1d55c8f3da578b0ec06d9684266
yiyangyi <yiyangyigg@gmail.com>
2015-03-20 22:02:36 +0800

    Add the google search method.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,10 @@
 class SearchController < ApplicationController
   def index
   end
+
+  def google
+  	keywords = params[:q] || ''
+  	keyword.gsub('#', '%23')
+  	redirect_to "https://www.google.com/#hl=zh-CN&q=site:cc98.org+#{keywords}"
+  end
 end
