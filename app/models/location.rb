acc61eae5569de19aa7eb9aafd716d3752fa91e5
yiyangyi <yiyangyigg@gmail.com>
2015-03-24 18:24:33 +0800

    Add the methods to find a location by name or if not exists create it.

--- app/models/location.rb
+++ app/models/location.rb
@@ -11,4 +11,18 @@ class Location
   index :name
 
   validates :name, uniqueness: true
+
+  def self.find_by_name(name)
+  	return false if name.blank?
+  	name = name.downcase.strip
+  	query = name.match(/\p{Han}/) != nil ? name : "#{name}"
+  	self.find(name: query).first
+  end
+
+  def self.find_or_create_by_name(name)
+  	if not location = self.find_by_name(name)
+  	  location = self.create(name: name.strip)
+  	end
+  	location
+  end
 end
