d6bbf33f08c0d725ee71aed1200a5fed210dfaa4
yiyangyi <yiyangyigg@gmail.com>
2015-03-24 20:21:34 +0800

    Add the notice hash including title, content and content_path.

--- app/models/notification/mention.rb
+++ app/models/notification/mention.rb
@@ -4,6 +4,12 @@ class Notification::Mention < Notification::Base
   delegate :body, to: :mentionable, prefix: true, allow_nil: false
 
   def notice_hash
+  	return if self.mentionable.blank?
+  	{
+  	  title: [self.mentionable.login, "提及你："].join(' '),
+  	  content: self.mentionable_body[0, 30],
+  	  content_path: self.content_path
+  	}
   end
 
   def content_path
--- app/models/notification/topic_reply.rb
+++ app/models/notification/topic_reply.rb
@@ -4,6 +4,12 @@ class Notification::TopicReply < Notification::Base
   delegate :body, to: :reply, prefix: true, allow_nil: false
 
   def notice_hash
+  	return '' if self.reply.blank?
+  	{
+  	  title: '您关注的话题有了新回复。'
+  	  content: self.reply_body[0, 30],
+  	  content_path: self.content_path
+  	}
   end
 
   def content_path
