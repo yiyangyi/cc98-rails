a9cd931b34000097fa78a1801101dea2736f32a9
yiyangyi <yiyangyigg@gmail.com>
2015-03-24 19:34:52 +0800

    Add two method to be implemented.

--- app/models/notification/mention.rb
+++ app/models/notification/mention.rb
@@ -2,4 +2,11 @@ class Notification::Mention < Notification::Base
   belongs_to :mentionable, polymorphic: true
 
   delegate :body, to: :mentionable, prefix: true, allow_nil: false
+
+  def notice_hash
+  end
+
+  def content_path
+  end
+  
 end
--- app/models/notification/topic_reply.rb
+++ app/models/notification/topic_reply.rb
@@ -2,4 +2,11 @@ class Notification::TopicReply < Notification::Base
   belongs_to :reply
 
   delegate :body, to: :reply, prefix: true, allow_nil: false
+
+  def notice_hash
+  end
+
+  def content_path
+  end
+
 end
