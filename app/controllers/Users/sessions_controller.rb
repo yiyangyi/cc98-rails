a43d2ddb0639a645f48c87d36bc5e33f3f27efa9
yiyangyi <yiyangyigg@gmail.com>
2015-03-25 14:37:35 +0800

    Add the method to log the user in and log the user out and set up the cookies.

--- app/controllers/Users/sessions_controller.rb
+++ app/controllers/Users/sessions_controller.rb
@@ -2,19 +2,31 @@ class Users::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      if params[:remember_me]
+        cookies.permanent.signed[:auth_token] = user.auth_token
+      else
+        cookies.signed[:auth_token] = user.auth_token
+      end
+      redirect_to root_url, notice: 'Logged in!'
+    else
+      flash.now.alert()
+      render :new
+    end
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    cookie.delete :auth_token
+    redirect_to root_url, notice: 'Logged out!'
+  end
 
   # protected
 
